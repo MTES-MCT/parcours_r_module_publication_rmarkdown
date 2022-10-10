47b5fef247393fe25e8fc14e1d29263b6a5ac735
laurent beltran <laurent.beltran@developpement-durable.gouv.fr>
2022-10-09 23:45:41 -0400

    Just what is needed as packages for book compilation

--- env-setup.R
+++ env-setup.R
@@ -2,28 +2,13 @@ renv::activate() # facultatif, nécessite 'renv'
 
 needed_packages <- c(
   
+  'bookdown',
+  
   'devtools',
   
-  'rmarkdown',
-  'distill',
-  'xaringan',
-  'xaringanExtra',
-  'xaringanthemer',
-  'pagedown',
-  'showtext',
-  'webshot2',
-  'metathis',
-  
-  'dplyr',
-  'ggplot2',
-  'COVID19',
-  'bit64',
-  'RSQLite',
-  'tidyr'
 )
 
 
 install.packages(needed_packages)
 
 devtools::install_github("hadley/emo")
-devtools::install_github("spyrales/gouvdown.fonts")
